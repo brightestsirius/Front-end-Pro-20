b612f80c69d1cdc5acbdc54db719145416d0d679
brightestsirius <katherina.shypovskaya@gmail.com>
2022-08-01 18:31:17 +0300

    lesson_3 – todo

---
+++ lesson_3/script.js
@@ -0,0 +1,13 @@
+// String to Number
+// alert, prompt
+// String methods
+// trim()
+// replace/replaceAll
+// toLowerCase/toUpperCase
+// slice
+// Boolean
+// if/else
+// >=; ===;
+// confirm
+// switch/case
+// break; continue
