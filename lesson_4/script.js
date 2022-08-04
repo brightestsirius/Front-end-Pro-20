d1776a31010312978d3f5f11d377ff1286a4ecb7
brightestsirius <katherina.shypovskaya@gmail.com>
2022-08-04 16:52:40 +0300

    lesson_4 – todo

---
+++ lesson_4/script.js
@@ -0,0 +1,16 @@
+// logic string
+// indexOf, includes, startsWith, endsWith
+
+// Boolean
+// if/else
+// >=; ===; !==;
+// &&, ||
+// confirm
+// isNaN
+
+// switch/case
+
+// while
+// do/while
+// for
+// break; continue
