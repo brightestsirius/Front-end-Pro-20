95de24d2df007e4ee45b8048166eddae1c5bfc4d
brightestsirius <katherina.shypovskaya@gmail.com>
2022-08-08 18:52:22 +0300

    lesson_5 – todo

---
+++ lesson_5/script.js
@@ -0,0 +1,17 @@
+// document.write()
+// confirm or prompt
+// if/else or if/if
+// let/const in if
+// single line
+
+// switch/case
+// ternar
+
+// while
+// do/while
+// for
+// break; continue;
+
+// debugger
+
+// array
