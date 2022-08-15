9710f08380cf52ba6578c2a29c3b25f9d8315529
brightestsirius <katherina.shypovskaya@gmail.com>
2022-08-15 18:26:23 +0300

    lesson_7 – todo

---
+++ lesson_7/script.js
@@ -0,0 +1,26 @@
+// HW && RegEx
+
+// Array
+
+// min/max element in array
+// min/max element in array by index
+// change position of min/max
+
+// Math.random()
+
+// Copy of array
+
+// Array methods
+//     push
+//     pop
+//     shift
+//     unshift
+//     reverse
+//     concat
+//     slice
+//     splice
+//     join
+//     split
+//     indexOf
+
+// Two dimensional array
