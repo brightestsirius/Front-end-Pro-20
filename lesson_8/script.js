bc860dbc3075143b1c395c3bc617976346f6025d
brightestsirius <katherina.shypovskaya@gmail.com>
2022-08-22 18:43:45 +0300

    lesson_8 – todo

---
+++ lesson_8/script.js
@@ -0,0 +1,19 @@
+// Copy of array
+
+// Array methods
+//     push
+//     pop
+//     shift
+//     unshift
+//     reverse
+//     concat
+//     slice
+//     splice
+//     join
+//     split
+
+// Two dimensional array
+
+// Array.isArray()
+
+// Chess
