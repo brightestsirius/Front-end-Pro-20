29de56110070ff47b5e388ec15189e5245d85c97
brightestsirius <katherina.shypovskaya@gmail.com>
2022-08-29 13:33:05 +0300

    lesson_10 – todo

---
+++ lesson_10/script.js
@@ -0,0 +1,19 @@
+// Lexical environment, Global/Local Scope
+// hoisting
+// closure
+// arguments
+// function as argument
+// callback
+// spread/rest in function
+// arrow function
+// recursion
+// setTimeout
+// setInterval/clearInterval
+
+// Array methods
+// forEach
+// filter
+// map
+// find/findIndex
+// every/some
+// reduce
