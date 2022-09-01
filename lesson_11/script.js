99d59e7cab66eb07839f0a954839b483c266c237
brightestsirius <katherina.shypovskaya@gmail.com>
2022-09-01 19:06:25 +0300

    lesson_11 – todo

---
+++ lesson_11/script.js
@@ -0,0 +1,18 @@
+// arguments
+// function as argument
+// callback
+// spread/rest in function
+// arrow function
+// setTimeout
+// setInterval/clearInterval
+// Event loop
+
+// Array methods
+// forEach
+// filter
+// map
+// find/findIndex
+// every/some
+// reduce
+
+// Object
