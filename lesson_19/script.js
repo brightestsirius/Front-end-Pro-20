578ad93b4a80e0e456d5de728df86fe1e8a954dc
brightestsirius <katherina.shypovskaya@gmail.com>
2022-10-03 18:54:58 +0300

    lesson_19 – todo

---
+++ lesson_19/script.js
@@ -0,0 +1,36 @@
+// attributes
+
+const block = document.querySelector(`#block`);
+console.dir(block);
+
+// dataset
+// createElement
+// remove
+// events
+
+// click events
+//     onclick
+//     ondblclick
+//     oncontextmenu
+//     onmousedown
+//     onmouseup
+
+// addEventListener/removeEventListener
+
+// mouse events
+//     onmouseover
+//     onmouseout
+//     onmouseenter
+//     onmouseleave
+//     onmousemove
+
+// keyboard events
+//     onkeypress
+//     onkeydown
+//     onkeyup
+
+// Bubbling and capturing
+
+// stopPropagation
+
+// preventDeafault()
