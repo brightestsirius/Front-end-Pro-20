adfbb59e374310afaee05e8307e0f23add6e4565
brightestsirius <katherina.shypovskaya@gmail.com>
2022-11-10 18:22:24 +0200

    lesson_29

---
+++ lesson_29/script.js
@@ -0,0 +1,72 @@
+// JS Modules
+
+// JQuery
+// selectors
+// $(blocks).each((index, block)=>{})
+// css
+// addClass, removeClass, toggleClass
+
+// .prev()
+// .next()
+// .siblings()
+// .parents()
+
+
+// .wrap();
+// .unwrap();
+
+// text()/html()
+// prepend()/append()
+// before()/after()
+// wrap()/unwrap()
+// empty(), remove(), clone()
+
+// actions/on
+
+// animation
+// hide/show/toggle
+// fadeIn()/fadeOut()/fadeToggle()
+// slideUp()/slideDown()/slideToggle(3000)
+
+// Ajax
+
+// const URL = `https://jsonplaceholder.typicode.com/posts`;
+
+// $.ajax({
+// 	url : URL,
+// 	method: 'GET',
+// 	success: data => console.log(data),
+// 	error: error => console.log(error)
+// })
+
+// або
+// $.ajax({
+// 		url : URL,
+// 		method: 'GET',
+//      error: error => console.log(`in catch:`, error)
+// 	})
+// 	.done(data => {
+// 		console.log(`in resolve 1:`, data);
+//      return data;
+// 	})
+//  .done(data => {
+// 		console.log(`in resolve 2:`, data);
+// 	});
+
+// POST
+// $.ajax({
+// 	url: URL,
+// 	type: "POST",
+// 	dataType: "JSON",
+// 	data: {title: "New title"},
+// 	success: data => console.log(data),
+// 	error: error => console.log(error)
+// });
+
+// NodeJS
+// http
+    // require()
+    // module.exports = {}
+
+// npm
+// node-static
